ac3ad80aa34254fa03cdb0b3c5b4fc78b759c1f0
“User <musagurbuz4661@gmail.com>
2022-06-23 12:44:29 +0200

    and or operators

---
+++ Batch81/Batch81/src/Day07_ifStatement/C01_AndOrOperators.java
@@ -0,0 +1,13 @@
+package Day07_ifStatement;
+
+public class C01_AndOrOperators {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        int c=20;
+
+        System.out.println(a>0 && b<20 && c>=b);
+
+        System.out.println(a<0 && b<20 && c>=b);
+    }
+}
