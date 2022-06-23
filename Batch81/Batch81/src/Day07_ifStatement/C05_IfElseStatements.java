e8c1d2454e5bb48602fad94297aed52fb6bbc7a5
“User <musagurbuz4661@gmail.com>
2022-06-23 15:22:11 +0200

    example if else

---
+++ Batch81/Batch81/src/Day07_ifStatement/C05_IfElseStatements.java
@@ -0,0 +1,23 @@
+package Day07_ifStatement;
+
+import java.util.Scanner;
+
+public class C05_IfElseStatements {
+    public static void main(String[] args) {
+         /*
+        Kullanicidan bir karakter girmesini isteyin
+        ve girilen karakterin harf olup olmadigini yazdirin
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir karakter giriniz");
+        char harf = scan.next().charAt(0);
+
+        if ((harf>='A' && harf<='Z')||(harf>='a' && harf<='z')){
+            System.out.println("Girdiginiz karakter bir harftir");
+        }else {
+            System.out.println("Girdiginiz karakter bir harf degildir");
+        }
+
+    }
+}
