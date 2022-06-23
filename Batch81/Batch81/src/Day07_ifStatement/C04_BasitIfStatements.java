475de40c05aee5dad387a21dbe3159378f0e2287
“User <musagurbuz4661@gmail.com>
2022-06-23 14:19:33 +0200

    hafta ici hafta sonu bul

--- Batch81/Batch81/src/Day07_ifStatement/C04_BasitIfStatements.java
+++ Batch81/Batch81/src/Day07_ifStatement/C04_BasitIfStatements.java
@@ -1,2 +1,33 @@
-package Day07_ifStatement;public class C04_BasitIfStatements {
+package Day07_ifStatement;
+
+import java.util.Scanner;
+
+public class C04_BasitIfStatements {
+
+    public static void main(String[] args) {
+        /*
+        Soru 3) Kullanicidan gun ismini alin ve haftaici veya hafta sonu oldugunu yazdirin
+        Ornek:      gun=Pazar output = “Hafta sonu”
+                    gun=Sali  output = “Hafta ici”
+         *** String icin equals method’unu kullanin
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir gun ismi giriniz :");
+        String gun = scan.next().toLowerCase();
+
+        if (gun.equals("pazar") || gun.equals("cumartesi")) {
+            System.out.println("Hafta Sonu");
+        }
+        if (gun.equals("pazartesi") || gun.equals("sali") || gun.equals("carsamba")
+                || gun.equals("persembe") || gun.equals("cuma")){
+            System.out.println("Hafta ici");
+        }
+        if (!(gun.equals("pazartesi") || gun.equals("sali") || gun.equals("carsamba")
+                || gun.equals("persembe") || gun.equals("cuma") ||gun.equals("pazar") || gun.equals("cumartesi"))){
+            System.out.println("Boyle bir gun yok");
+        }
+
+
+    }
 }
