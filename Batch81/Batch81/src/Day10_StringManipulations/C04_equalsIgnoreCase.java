d3f72f29cadb2b501dafe09cf58df1f699d4ce9f
“User <musagurbuz4661@gmail.com>
2022-06-24 16:55:29 +0200

    equalsIgnoreCase

--- Batch81/Batch81/src/Day10_StringManipulations/C04_equalsIgnoreCase.java
+++ Batch81/Batch81/src/Day10_StringManipulations/C04_equalsIgnoreCase.java
@@ -9,5 +9,11 @@ public class C04_equalsIgnoreCase {
 
         System.out.println(str1.equals(str2)); //false
         System.out.println(str1.equalsIgnoreCase(str2)); //true
+
+        System.out.println(str2.equals(str3)); // false
+        System.out.println(str2.equalsIgnoreCase(str3)); // true3
+        
+        System.out.println(str1.equals(str4)); // false
+        System.out.println(str1.equalsIgnoreCase(str4)); // false
     }
 }
