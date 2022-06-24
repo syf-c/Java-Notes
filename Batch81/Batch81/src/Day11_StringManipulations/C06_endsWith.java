49ffba1ed97e4cdf806bf6b891c44f0571478140
“User <musagurbuz4661@gmail.com>
2022-06-24 22:10:27 +0200

    endsWith

---
+++ Batch81/Batch81/src/Day11_StringManipulations/C06_endsWith.java
@@ -0,0 +1,24 @@
+package Day11_StringManipulations;
+
+import java.util.Scanner;
+
+public class C06_endsWith {
+    public static void main(String[] args) {
+        /*
+        Soru 1) Kullanicidan email adresini girmesini isteyin, mail @gmail.com
+        icermiyorsa  "lutfen gmail adresi giriniz",
+        @gmail.com ile bitiyorsa  "Email adresiniz kaydedildi " ,
+        @gmail.com ile bitmiyorsa lutfen yazimi kontol edin yazdirin
+         */
+        String input="musa@gmail.com";
+
+        if (!input.contains("@gmail.com")){
+            System.out.println("lutfen gmail adresi giriniz");
+        }else if(input.endsWith("@gmail.com")){
+            System.out.println("Emailiniz kaydedildi");
+        }else {
+            System.out.println("lutfen yazimi kontrol edin");
+        }
+    }
+
+}
