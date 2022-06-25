40dc906c8eb8ebb708807f35322075e9d39ea4b8
“User <musagurbuz4661@gmail.com>
2022-06-25 16:00:44 +0200

    contains

---
+++ Batch81/Batch81/src/Day11_StringManipulations/Soru3.java
@@ -0,0 +1,18 @@
+package Day11_StringManipulations;
+
+import java.util.Scanner;
+
+public class Soru3 {
+    //Soru 1) Kullanicidan email adresini girmesini isteyin,
+    // mail @gmail.com icermiyorsa  "lutfen gmail adresi giriniz",
+    // @gmail.com ile bitiyorsa  "Email adresiniz kaydedildi " ,
+    // @gmail.com ile bitmiyorsa lutfen yazimi kontol edin yazdirin
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+
+
+
+    }
+}
