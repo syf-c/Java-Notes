ee94dadabc672591f11632a19c4a84ee24ef3ea5
“User <musagurbuz4661@gmail.com>
2022-06-26 11:27:26 +0200

    String Manipulation contains

--- Batch81/Batch81/src/Day11_StringManipulations/Soru3.java
+++ Batch81/Batch81/src/Day11_StringManipulations/Soru3.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class Soru3 {
     //Soru 1) Kullanicidan email adresini girmesini isteyin,
-    // mail @gmail.com icermiyorsa  "lutfen gmail adresi giriniz",
+    // email "@gmail.com" icermiyorsa  "lutfen gmail adresi giriniz",
     // @gmail.com ile bitiyorsa  "Email adresiniz kaydedildi " ,
     // @gmail.com ile bitmiyorsa lutfen yazimi kontol edin yazdirin
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Email adresinizi giriniz");
+        String email =scan.nextLine();
 
+        if (!email.contains("@gmail.com")){
+            System.out.println("lutfen gmail adresi giriniz");
+        }else if(email.contains("@gamil.com")){
+            System.out.println("Email adresiniz kaydedildi");
+        }else{
+            System.out.println("lutfen yazimi kontrol edin");
+        }
 
 
     }
