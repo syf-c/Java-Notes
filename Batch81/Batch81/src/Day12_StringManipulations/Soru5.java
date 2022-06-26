b2d11c6415fd5bfa8545d1a17236d4c8808ab4b7
“User <musagurbuz4661@gmail.com>
2022-06-26 20:43:40 +0200

    String manipulation

---
+++ Batch81/Batch81/src/Day12_StringManipulations/Soru5.java
@@ -0,0 +1,22 @@
+package Day12_StringManipulations;
+
+import java.util.Scanner;
+
+public class Soru5 {
+    public static void main(String[] args) {
+        /*
+        kullanicidan 4 harfli bir kelime isteyin ve girilen kelimeyi tersten yazdirin
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("4 hafli bir kelime giriniz");
+        String kelime = scan.next();
+
+        if (kelime.length() ==4 ) {
+            System.out.println(kelime.substring(3,4)+kelime.substring(2,3)+kelime.substring(1,2)+kelime.substring(0,1));
+        }else{
+            System.out.println("lutfen 4 harfli bir kelime giriniz");
+        }
+
+    }
+}
