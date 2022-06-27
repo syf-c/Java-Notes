2241a58f317197673507d853779f023e028df1f8
“User <musagurbuz4661@gmail.com>
2022-06-27 12:39:02 +0200

    string manipulation

--- Java_Notes/Java_Notes/src/Day12_StringManipulations/Soru6.java
+++ Java_Notes/Java_Notes/src/Day12_StringManipulations/Soru6.java
@@ -18,36 +18,42 @@ public class Soru6 {
         System.out.println("Lutfen bir sifre giriniz");
         String sifre =scan.next();
 
-        if((sifre.substring(0,1)!=sifre.substring(0,1).toUpperCase())){
-
+        if((!sifre.substring(0, 1).toUpperCase().equals(sifre.substring(0, 1)))){
             System.out.println("ok1");
         }
-       if (sifre.substring(sifre.length()-1)!=
-                sifre.substring(sifre.length()-1).toLowerCase()){
+       if (!sifre.substring(sifre.length()-1).equals(sifre.substring(sifre.length()-1).toLowerCase())){
             System.out.println("ok2");
         }
-
         if (sifre.contains(" ") !=sifre.contains("")){
             System.out.println("ok3");
         }
+        if (sifre.length() >8) System.out.println("ok4");
 
 
-
-        if (sifre.length() >8){
-            System.out.println("ok4");
-        }
-
-
-
-
-        if ((sifre.substring(0,1)!=sifre.substring(0,1).toUpperCase()&&
-                sifre.substring(sifre.length()-1)!=sifre.substring(sifre.length()-1).toLowerCase()&&
+        if ((!sifre.substring(0,1).equals(sifre.substring(0,1).toUpperCase())&&
+                !sifre.substring(sifre.length()-1).equals(sifre.substring(sifre.length()-1).toLowerCase())&&
                 sifre.contains(" ")!=sifre.contains("")&& sifre.length()>8)
         ){
             System.out.println("isleminiz basarili");
         }else {
             System.out.println("islem basarisiz ");
         }
+
+
+
+        /* ********************** IKINCI COZUM*******************
+
+        boolean ilkHarfKontrol=sifre.charAt(0)>='A' && sifre.charAt(0)<='Z';
+        boolean sonHarfKontrol=sifre.charAt(sifre.length()-1)>='a' && sifre.charAt(sifre.length()-1)<='z';
+        boolean spaceKontrol=!sifre.contains(" ");
+        boolean uzunlukKontrol=sifre.length()>=8;
+        if (ilkHarfKontrol && sonHarfKontrol && spaceKontrol && uzunlukKontrol){
+            System.out.println("Sifre basari ile tanimlandi");
+        }else {
+            System.out.println("Islem basarisiz,Lutfen yeni bir sifre girin");
+        }
+
+         */
 
     }
 }
