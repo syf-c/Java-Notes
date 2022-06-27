1d46f1591844a98cc43b61e6a85e453440ad80c5
“User <musagurbuz4661@gmail.com>
2022-06-27 13:36:18 +0200

    Q101_IfStatement02

---
+++ Practice question days/src/Day1/Q11_IfStatement02.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Q11_IfStatement02 {
+    /*
+    not hesaplayici
+		   	A => 90 ~100
+		   	B => 80 ~ 89
+		   	C => 70 ~ 79
+		   	D => 60 ~ 69
+		   	F =>  0 ~ 59
+     */
+
+
+
+}
