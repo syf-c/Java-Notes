158f153673b2f40e41d50a6ab4ade9e1ea1a0e4f
“User <musagurbuz4661@gmail.com>
2022-06-27 13:36:40 +0200

    Q12_IfStatement03

---
+++ Practice question days/src/Day1/Q12_IfStatement03.java
@@ -0,0 +1,19 @@
+package Day1;
+
+
+import java.util.Scanner;
+
+public class Q12_IfStatement03 {
+		/*
+        Print "Lutfen is unvaninizi girin
+        jobTitle isimli bir degisken olusturun ve kullanicidan isteyin.
+        Dogru jobTitle yazdirmak icin asagidaki test datalarini kullanin. Example :
+        Eger jobTitle  qa ise print is unvaniniz Quality Analyst
+        test data: qa ise print Quality Analyst
+        dev ise print Developer
+        ba ise print Business Analyst
+        pm ise print Project Manager
+       */
+
+}
+
