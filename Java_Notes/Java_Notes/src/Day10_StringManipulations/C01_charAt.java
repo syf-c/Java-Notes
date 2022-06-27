5da1feb37cdfe301f8356502cda622051d58f252
“User <musagurbuz4661@gmail.com>
2022-06-27 12:50:36 +0200

    String manipulation methods

--- Java_Notes/Java_Notes/src/Day10_StringManipulations/C01_charAt.java
+++ Java_Notes/Java_Notes/src/Day10_StringManipulations/C01_charAt.java
@@ -2,7 +2,7 @@ package Day10_StringManipulations;
 
 public class C01_charAt {
     public static void main(String[] args) {
-        String str="Java ogrenmek ne guzel";
+        String str="Java ogrenmek ne guzeldir";
 
         System.out.println(str.charAt(0)); // ilk harfi yazdirir J
         System.out.println(str.toUpperCase().charAt(7)); // R
