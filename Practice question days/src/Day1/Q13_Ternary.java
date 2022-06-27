b1ee2fa73af19c24be5fc6a780363c5266d73423
“User <musagurbuz4661@gmail.com>
2022-06-27 13:37:15 +0200

    Q13_Ternary

---
+++ Practice question days/src/Day1/Q13_Ternary.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Q13_Ternary {
+    /*
+     * Ternary kullanarak bir kod yazin. fiyat isminde bir variable tanimlayin. eger fiyat
+     * 10 dan az ise "ucuz" , 10 ile 20 arasinda ise "normal" , 20 ve 20 den buyuk ise pahali
+     * olsun
+     */
+
+}
