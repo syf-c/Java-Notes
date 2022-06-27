84daa1cf9a5c6a9b1d5d87c2c0643cc5fcd4993f
“User <musagurbuz4661@gmail.com>
2022-06-27 13:35:51 +0200

    Q10_IfStatement01

---
+++ Practice question days/src/Day1/Q10_IfStatement01.java
@@ -0,0 +1,13 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Q10_IfStatement01 {
+    /*
+     * KullanicidanY/N ikilisinden birisini girdiginde girdigi degeri
+     * ekrana yazdiran java kodunu yaziniz.
+     * INPUT : Y ,  N
+     * OUTPUT : YES ; NO
+     */
+
+}
