35c68c3a7f8e2a7b861288050efbce759b454937
“User <musagurbuz4661@gmail.com>
2022-06-27 20:03:46 +0200

    Q10_IfStatement01

--- Practice question days/src/Day1/Q10_IfStatement01.java
+++ Practice question days/src/Day1/Q10_IfStatement01.java
@@ -9,5 +9,20 @@ public class Q10_IfStatement01 {
      * INPUT : Y ,  N
      * OUTPUT : YES ; NO
      */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" yes icin Y no icin N e tiklayiniz : " );
+        String secim = scan.next().toLowerCase();
+
+        if (secim.equals("y")){
+            System.out.println("Yes");
+        }else if (secim.equals("n")){
+            System.out.println("No");
+        }else {
+            System.out.println("lutfen Y veya N seciniz");
+        }
+
+
+    }
 
 }
