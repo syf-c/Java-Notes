77ac23df6181901b7a48023c00408150cc8b6c8d
“User <musagurbuz4661@gmail.com>
2022-06-27 20:21:06 +0200

    Q11_IfStatement02

--- Practice question days/src/Day1/Q11_IfStatement02.java
+++ Practice question days/src/Day1/Q11_IfStatement02.java
@@ -1,5 +1,7 @@
 package Day1;
 
+import java.util.Scanner;
+
 public class Q11_IfStatement02 {
     /*
     not hesaplayici
@@ -9,7 +11,25 @@ public class Q11_IfStatement02 {
 		   	D => 60 ~ 69
 		   	F =>  0 ~ 59
      */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen notunuzu giriniz :");
+        int note = scan.nextInt();
 
+        if (note < 0 || note > 100) {
+            System.out.println("lutfen 0 ile 100 arasinda bir not giriniz");
+        } else if (note <= 59) {
+            System.out.println("Notunuz F dir");
+        } else if (note <= 69) {
+            System.out.println("Notunuz D dir");
+        } else if (note <= 79) {
+            System.out.println("Notunuz C dir");
+        } else if (note < 89) {
+            System.out.println("Notunuz B dir");
+        } else {
+            System.out.println("Notunuz A dir");
+        }
+    }
 
 
 }
