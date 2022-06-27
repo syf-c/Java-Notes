9a51ffef9018a8500b71309065f280b567bae899
“User <musagurbuz4661@gmail.com>
2022-06-27 20:57:06 +0200

    12_IfStatement03

--- Practice question days/src/Day1/Q12_IfStatement03.java
+++ Practice question days/src/Day1/Q12_IfStatement03.java
@@ -14,6 +14,23 @@ public class Q12_IfStatement03 {
         ba ise print Business Analyst
         pm ise print Project Manager
        */
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Lutfen is unvaninizi giriniz :");
+            String jobTitle =scan.nextLine().toLowerCase();
+
+            if (jobTitle.equals("qa")){
+                System.out.println("Quality Analyst");
+            }else if (jobTitle.equals("dev")){
+                System.out.println("Developer");
+            }else if (jobTitle.equals("ba")){
+                System.out.println("Business Analyst");
+            }else if (jobTitle.equals("pm")){
+                System.out.println("Project Manager");
+            }else{
+                System.out.println("lutfen gecerli bir unvan giriniz ..");
+            }
+        }
 
 }
 
