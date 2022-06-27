32372806cc9c62c9d46d79279794dc91fbabc584
“User <musagurbuz4661@gmail.com>
2022-06-27 19:53:48 +0200

    Q09_Scanner02

--- Practice question days/src/Day1/Q09_Scanner02.java
+++ Practice question days/src/Day1/Q09_Scanner02.java
@@ -10,8 +10,36 @@ public class Q09_Scanner02 {
     // next() only can read the first word
     // nextLine() can read the whole line
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen adinizi giriniz :");
+        String isim = scanner.nextLine();
+
+        System.out.println("memleketiniz : ");
+        String memleket = scanner.nextLine();
+
+        System.out.println("Konumunuz : ");
+        String konum = scanner.nextLine();
+
+        System.out.println("yasinizi giriniz :");
+        int yas = scanner.nextInt();
+
+        System.out.println("Boyunuzu giriniz :");
+        double boy = scanner.nextDouble();
+
+        System.out.println("yasadiginiz "+ konum+ " seviyormusunuz? true/false");
+        boolean seviyorMu=scanner.nextBoolean();
+
+        System.out.println("isim = " + isim);
+        System.out.println("memleket = " + memleket);
+        System.out.println("konum = " + konum);
+        System.out.println("yas = " + yas);
+        System.out.println("boy = " + boy);
+        System.out.println("seviyorMu = " + seviyorMu);
 
 
 
-
+    }
 }
+
+
