db94c051941e9ac81dacaeb9b7618857f978a258
“User <musagurbuz4661@gmail.com>
2022-06-27 12:44:35 +0200

    variables

--- Java_Notes/Java_Notes/src/Day01_variables/C01_Variables.java
+++ Java_Notes/Java_Notes/src/Day01_variables/C01_Variables.java
@@ -2,6 +2,6 @@ package Day01_variables;
 
 public class C01_Variables {
     public static void main(String[] args) {
-        System.out.println("Welcome to Batch81");
+        System.out.println("Welcome to Batch81 java lesson");
     }
 }
