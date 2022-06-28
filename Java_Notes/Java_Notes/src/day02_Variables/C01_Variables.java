d71f456f9f0217e2a88d88dc617998505387346b
“User <musagurbuz4661@gmail.com>
2022-06-28 12:09:53 +0200

    Variables

---
+++ Java_Notes/Java_Notes/src/day02_Variables/C01_Variables.java
@@ -0,0 +1,14 @@
+package day02_Variables;
+
+public class C01_Variables {
+    public static void main(String[] args) {
+        int not = 60;
+        int not2 = 80;
+        not2 = 90;
+        int ort = (not + not2)/2;
+        double sonuc = 25/5 ;
+        System.out.println("notunuz = "+ not + " not2 = " +not2);
+        System.out.println("ortalama = " + ort);
+        System.out.println(sonuc);
+    }
+}
---
+++ Java_Notes/Java_Notes/src/day02_Variables/C02_variables.java
@@ -0,0 +1,15 @@
+package day02_Variables;
+
+public class C02_variables {
+    public static void main(String[] args) {
+        int sayi = 10;
+        boolean guzelMi = true;
+        char sayim = '3';
+
+        System.out.println(sayi + sayim);
+
+        System.out.println("sayi = " + sayi);
+        System.out.println("guzel Mi  = " + guzelMi);
+        System.out.println("sayim = " + sayim);
+    }
+}
