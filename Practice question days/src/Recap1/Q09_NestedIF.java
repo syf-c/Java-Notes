de88538b139b796b32d3c79111c5281bcb4053f7
“User <musagurbuz4661@gmail.com>
2022-06-29 11:41:05 +0200

    Q09_NestedIF

---
+++ Practice question days/src/Recap1/Q09_NestedIF.java
@@ -0,0 +1,15 @@
+package Recap1;
+
+public class Q09_NestedIF {
+    public static void main(String[] args) {
+
+    /*
+    Kullanici dan yas bilgisini alarak
+	    oy kullanma yasi :
+	 	age >= 18  ==> oy kullanmaya uygun
+	 	age >= 70  ==> uc kez oy kullanabilir
+	 	70 > age >=50 ==> iki kez oy kullanabilir
+	 	50 > age >=18 ==> bir kez oy kullanabilir, yazdiriniz
+	 */
+    }
+}
