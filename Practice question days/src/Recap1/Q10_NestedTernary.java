ec5bddb9e7bad5565c4d7faee8bc7bca909b6264
“User <musagurbuz4661@gmail.com>
2022-06-30 11:07:06 +0200

    NestedTernary

--- Practice question days/src/Recap1/Q10_NestedTernary.java
+++ Practice question days/src/Recap1/Q10_NestedTernary.java
@@ -1,22 +1,28 @@
 package Recap1;
 
+
 import java.util.Scanner;
 
 public class Q10_NestedTernary {
-      /*
-    Final notu tanimlayin, Nested Ternary ile cozunuz
-    'A'  ->  "Gayet Basarili"
-    'B'  ->  "Basarili"
-    'C'  ->   "Ha gayret"
-    bu notlar disindakilere de Digerleri.. yazdiriniz
-     */
-      public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
-          System.out.println("lutfen notunuzu giriniz :");
-          String
-          Sonuc= Sonuc.equals("A")? "Gayet basarili":(Sonuc.equals("B")? "Basarili":
-                 Sonuc.equals("C")?"Ha gayret":"Cok calismalisin");
-          System.out.println("finalSonuc = " + Sonuc);
-      }
-    
+    /*
+  Final notu tanimlayin, Nested Ternary ile cozunuz
+  'A'  ->  "Gayet Basarili"
+  'B'  ->  "Basarili"
+  'C'  ->   "Ha gayret"
+  bu notlar disindakilere de Digerleri.. yazdiriniz
+   */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen notunuzu giriniz");
+        String sonuc =scanner.next().toUpperCase().substring(0);
+        sonuc = sonuc.equals("A") ? "Gayet Basarili"
+                : (sonuc == "B" ? "Basarili"
+                :(sonuc == "C" ? "Ha gayret"
+                :(sonuc=="D"? "Az daha dikkat"
+                :(sonuc=="E" ? "Dikkatli olmalisin":"lutfen gecerli bir note giriniz"))));
+
+        System.out.println("finalNotu = " + sonuc);
+
+    }
+
 }
