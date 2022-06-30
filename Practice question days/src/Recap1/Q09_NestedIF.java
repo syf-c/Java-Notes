5f2204d0fdffb19a07a8ddf18ca265b09f955e6b
“User <musagurbuz4661@gmail.com>
2022-06-30 11:06:30 +0200

    Q09_NestedIF

--- Practice question days/src/Recap1/Q09_NestedIF.java
+++ Practice question days/src/Recap1/Q09_NestedIF.java
@@ -1,5 +1,7 @@
 package Recap1;
 
+import java.util.Scanner;
+
 public class Q09_NestedIF {
     public static void main(String[] args) {
 
@@ -11,5 +13,22 @@ public class Q09_NestedIF {
 	 	70 > age >=50 ==> iki kez oy kullanabilir
 	 	50 > age >=18 ==> bir kez oy kullanabilir, yazdiriniz
 	 */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen yasinizi giriniz");
+        int yas = scan.nextInt();
+
+        if (yas >= 18) {
+              if ( yas <= 50) {
+                System.out.println("birkez oy kullanabilir");
+            } else {
+                if (yas > 70) {
+                    System.out.println("uc kez oy kullanabilir");
+                } else {
+                    System.out.println("iki kez oy kullanabilir");
+                }
+              }
+        }else {
+            System.out.println("yasiniz 18'den kucuk oy kullanamazsiniz");
+        }
     }
 }
