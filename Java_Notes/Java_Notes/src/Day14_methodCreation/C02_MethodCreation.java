b18468544a2e08ff783bb573a5b2e09ed7617cba
“User <musagurbuz4661@gmail.com>
2022-06-30 20:55:07 +0200

    MethodCreation

--- Java_Notes/Java_Notes/src/Day14_methodCreation/C02_MethodCreation.java
+++ Java_Notes/Java_Notes/src/Day14_methodCreation/C02_MethodCreation.java
@@ -1,14 +1,18 @@
 package Day14_methodCreation;
 
+import Day13_MethodCreation.C04_MethodCreation;
+
 public class C02_MethodCreation {
     public static void main(String[] args) {
     //verilen 3 basamakli bir sayinin rakamlari toplamini yazdiran bir method olusturalim
         int input = 423;
         rakamlarToplami(input);
+        C04_MethodCreation.topla(4,5);
     }
     public static void rakamlarToplami(int input){
         int birlerBasamagi =0;
         int rakamlarToplami = 0;
+        int temp = input;
 
         birlerBasamagi = input%10;
         rakamlarToplami += birlerBasamagi;
@@ -22,7 +26,7 @@ public class C02_MethodCreation {
         rakamlarToplami += birlerBasamagi;
         input /=10;
 
-        System.out.println("girdiginiz input sayisinin rakamlar toplami : "+rakamlarToplami);
+        System.out.println("girdiginiz " + temp + " sayisinin rakamlar toplami : "+rakamlarToplami);
 
     }
 
