2d9ff83b239ee512db85373300c0914644254f59
“User <musagurbuz4661@gmail.com>
2022-07-01 17:28:44 +0200

    MethodCreation

--- Java_Notes/Java_Notes/src/Day14_methodCreation/Method01.java
+++ Java_Notes/Java_Notes/src/Day14_methodCreation/Method01.java
@@ -2,11 +2,17 @@ package Day14_methodCreation;
 
 public class Method01 {
     public static void main(String[] args) {
-        toplama(12,33,33);
+       // toplama(12,33,33);
+    //   int sonuc = toplama(1,2,3);
+     //   System.out.println(sonuc);
+        System.out.println("sayilarin toplami : "+toplama(1,2,3));
     }
 
-    public static void toplama(int a,int b,int c) {
-        System.out.println("toplamlari : "+(a+b+c));
+    public static int toplama(int a,int b,int c) {
+
+       // System.out.println("toplamlari : "+(a+b+c));
+
+        return   a+b+c;
     }
 
 
