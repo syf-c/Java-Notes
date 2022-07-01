abab462dc988ef5aad8563fe3c6c0e3db54f2db0
“User <musagurbuz4661@gmail.com>
2022-07-01 17:12:39 +0200

    MethodCreation

---
+++ Java_Notes/Java_Notes/src/Day14_methodCreation/Method01.java
@@ -0,0 +1,13 @@
+package Day14_methodCreation;
+
+public class Method01 {
+    public static void main(String[] args) {
+        toplama(12,33,33);
+    }
+
+    public static void toplama(int a,int b,int c) {
+        System.out.println("toplamlari : "+(a+b+c));
+    }
+
+
+}
