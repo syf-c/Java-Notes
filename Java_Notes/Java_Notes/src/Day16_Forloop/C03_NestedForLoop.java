19c952dc6b03c2ae1af4224bb079043ac620c65e
“User <musagurbuz4661@gmail.com>
2022-07-02 18:48:03 +0200

    NestedForLoop

---
+++ Java_Notes/Java_Notes/src/Day16_Forloop/C03_NestedForLoop.java
@@ -0,0 +1,12 @@
+package Day16_Forloop;
+
+public class C03_NestedForLoop {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+          for (int j=1;j<=i;j++){
+              System.out.print("* ");
+          }
+            System.out.println(" ");
+        }
+    }
+}
