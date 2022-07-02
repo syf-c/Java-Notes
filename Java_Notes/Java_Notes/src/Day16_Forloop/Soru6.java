2733ea106aa3cec6a2f2a1da256b923395626d12
“User <musagurbuz4661@gmail.com>
2022-07-02 18:17:17 +0200

    forloop

---
+++ Java_Notes/Java_Notes/src/Day16_Forloop/Soru6.java
@@ -0,0 +1,33 @@
+package Day16_Forloop;
+
+import java.util.Scanner;
+
+public class Soru6 {
+    public static void main(String[] args) {
+        /*
+  Soru 6) Interview Question Kullanicidan 100’den kucuk bir tamsayi isteyin.
+  1’den baslayarak girilen sayiya kadar tum sayilari yazdirin. Ancak;
+- Sayi 3’un kati ise sayi yerine "Java" yazdirin.
+- Sayi 5’in kati ise sayi yerine "Guzeldir" yazdirin.
+- Sayi hem 3’un hem 5’in kati ise sayi yerine "Java Guzeldir" yazdirin.
+
+   */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bir sayi giriniz :");
+        int sayi = scan.nextInt();
+
+        for (int i = 1; i <=sayi ; i++) {
+            if (i%3==0 && i%5==0){
+                System.out.println("Java guzeldir" +" ");
+            }else if(i%3==0){
+                System.out.println("Java" + " ");
+            }else if (i%5==0){
+                System.out.println("Guzeldir" +" ");
+            }
+            else{
+                System.out.println(i + " ");
+            }
+        }
+
+    }
+}
