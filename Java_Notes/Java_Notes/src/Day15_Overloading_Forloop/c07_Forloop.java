39ce3cffabe88cce39bc7facfe4fa2596863fe44
“User <musagurbuz4661@gmail.com>
2022-07-02 15:28:42 +0200

    Forloop

---
+++ Java_Notes/Java_Notes/src/Day15_Overloading_Forloop/c07_Forloop.java
@@ -0,0 +1,14 @@
+package Day15_Overloading_Forloop;
+
+public class c07_Forloop {
+    public static void main(String[] args) {
+        //100 den 1'e kadar 8 ile bolunebilen sayilari yazdirin
+        for (int i = 100; i >=1 ; i--) {
+            if(i%8==0){
+                System.out.println(i + " sayisi 8 ile tam bolunur");
+            }
+
+
+        }
+    }
+}
