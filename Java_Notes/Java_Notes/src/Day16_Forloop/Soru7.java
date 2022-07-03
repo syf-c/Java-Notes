e48c5a748150e586e6ee4b5da0b6cfe4e6327ce4
“User <musagurbuz4661@gmail.com>
2022-07-03 10:20:34 +0200

    tersten yazdir

---
+++ Java_Notes/Java_Notes/src/Day16_Forloop/Soru7.java
@@ -0,0 +1,18 @@
+package Day16_Forloop;
+
+import java.util.Scanner;
+
+public class Soru7 {
+    public static void main(String[] args) {
+        // kullanicidan bir String istseyin ve Stringi tersten yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bir kelime giriniz :");
+        String kelime = scanner.nextLine();
+        String sonHarf= " ";
+        for (int i =kelime.length()-1;i>=0 ; i--) {
+            sonHarf +=kelime.substring(i,i+1);
+        } System.out.println("tersten yazilimi :" + sonHarf);
+
+    }
+}
