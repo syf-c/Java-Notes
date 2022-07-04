c6cb42fc628150664aa5d264fd79e17dbec6f8ed
“User <musagurbuz4661@gmail.com>
2022-07-04 14:19:26 +0200

    replit question and answer

---
+++ Replit/src/replit/Soru_13.java
@@ -0,0 +1,39 @@
+package replit;
+
+import java.util.Scanner;
+
+public class Soru_13 {
+    /*
+    Write a Java program to compute and print sum of two given numbers (more than or equal to zero).
+
+If given integers or the sum have more than 10 digits, print "overflow".
+
+EXAMPLE:
+
+INPUT :
+Input two integers:
+
+25
+
+46
+
+OUTPUT :
+
+Sum of the numbers: 71
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter two integer");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int sum = num1 + num2 ;
+        if((sum) >= 1000000000 ) {
+            System.out.println("Overflow");
+        }else {
+            System.out.println("Sum of the numbers: " + (num1+num2));
+        }
+
+
+
+    }
+}
