a71e07b164ef5d4042ee2af93b677faa17941008
“User <musagurbuz4661@gmail.com>
2022-07-04 12:32:49 +0200

    Method creation

---
+++ Java_Notes/Java_Notes/src/Day14_methodCreation/Soru1.java
@@ -0,0 +1,55 @@
+package Day14_methodCreation;
+
+import java.util.Scanner;
+
+public class Soru1 {
+    //  kullanicidan bir sayi alin,
+    //  -bu sayinin tekmi ciftmi oldugunu
+    //  -sifirdan buyuk mu kucukmu oldugunu
+    //  -eger 100 den buyukse rakamlar toplamini
+    //  -yuden kucukse sadece birler basamagini yazdiran 3 method olusturun
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz :");
+        int sayi = scan.nextInt();
+
+        tekmiCiftmi(sayi);
+        sifirdanBuyKuc(sayi);
+        yudenBuyKuc(sayi);
+    }
+
+    private static void yudenBuyKuc(int sayi) {
+        int rakamToplami =0;
+        int birlerBas =0;
+
+        if(sayi>100){
+            while(sayi>0){
+                birlerBas = sayi%10;
+                rakamToplami += birlerBas;
+                sayi /=10;
+           }System.out.println("Rakamlar toplami : " + rakamToplami);
+
+        }else {
+            birlerBas = sayi%10;
+            System.out.println("Birler basamagi : "+birlerBas);
+        }
+    }
+
+    private static void sifirdanBuyKuc(int sayi) {
+        if(sayi>0){
+            System.out.println("Sayi sifirdan buyuktur");
+        }else if(sayi==0){
+            System.out.println("Sayi sifira esittir");
+        }else System.out.println("Sayi sifirdan kucuktur");
+    }
+
+    private static void tekmiCiftmi(int sayi) {
+        if (sayi%2==0){
+            System.out.println("Sayi ciftir");
+        }else{
+            System.out.println("Sayi tektir");
+        }
+    }
+
+}
