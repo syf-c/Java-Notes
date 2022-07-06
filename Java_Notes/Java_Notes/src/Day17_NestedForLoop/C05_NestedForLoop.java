90b717a43e72157c2c26cc6a341ba3692964b916
“User <musagurbuz4661@gmail.com>
2022-07-06 12:09:59 +0200

    Nested forLoop

---
+++ Java_Notes/Java_Notes/src/Day17_NestedForLoop/C05_NestedForLoop.java
@@ -0,0 +1,32 @@
+package Day17_NestedForLoop;
+
+public class C05_NestedForLoop {
+    /*
+    verilen input'a gore * lardan
+    *
+    * *
+    * * *
+    * * * *
+    * * *
+    * *
+    *
+     */
+
+    // artan kismi nestedforloop ile yapalim
+    public static void main(String[] args) {
+        int input=4;
+        for (int i = 1; i <=input ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }for (int i =input-1 ; i >=1 ; i--) {
+            for (int j = i; j >=1 ; j--) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+}
