799badb3a482c9bec76e81b0b75f26796d190330
“User <musagurbuz4661@gmail.com>
2022-07-06 19:14:42 +0200

    Nested For loop

--- Java_Notes/Java_Notes/src/Day17_NestedForLoop/C06_NestedForLoop.java
+++ Java_Notes/Java_Notes/src/Day17_NestedForLoop/C06_NestedForLoop.java
@@ -2,7 +2,7 @@ package Day17_NestedForLoop;
 
 public class C06_NestedForLoop {
     public static void main(String[] args) {
-         String input = "TURKIYE";
+         String input = "TURKIYEM";
 
         for (int i = 1; i <=input.length() ; i++) {
             for (int j = 1; j <=i ; j++) {
@@ -10,9 +10,10 @@ public class C06_NestedForLoop {
             }
             System.out.println(" ");
         }
-        for (int i = input.length(); i <1; i--) {
-            for (int j = i; j <=1 ; j--) {
-                System.out.print(input.substring(j,j+1)); //ilk harf icin substring(0,1) olmali
+
+        for (int i = input.length()-1; i >=1; i--) {
+            for (int j =1; j <=i; j++) {
+                System.out.print(input.substring(j-1,j)); //ilk harf icin substring(0,1) olmali
             }
             System.out.println(" ");
         }
