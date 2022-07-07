21a01f1447aa004bbd86834ca3f715388623fa72
“User <musagurbuz4661@gmail.com>
2022-07-07 13:00:04 +0200

    Do While loop

---
+++ Java_Notes/Java_Notes/src/Day18_While_Do_WhileLoop/Soru6_TamBolenler.java
@@ -0,0 +1,38 @@
+package Day18_While_Do_WhileLoop;
+
+public class Soru6_TamBolenler {
+    /*
+    Soru 6) Kullanicidan bir sayi alin ve bu sayiyi tam bolen sayilari
+    ve toplam kac tane olduklarini ekranda yazdirin
+     */
+    public static void main(String[] args) {
+        int sayi = 125;
+        int bolenlerAdet = 0;
+        System.out.print("tam bolen sayilar : ");
+        //-------For loop---------------------------------------------------
+        for (int i = 1; i <= sayi; i++) {
+            if (sayi % i == 0) {
+                System.out.print(i + " ");
+                bolenlerAdet += 1;
+            }
+        }
+        System.out.println("");
+        System.out.println(bolenlerAdet + " Adet boleni vardir");
+
+
+        //-------------While loop-------------------------------------------
+        int bas = 1;
+        bolenlerAdet = 0;
+        System.out.print("tam bolen sayilar : ");
+        while (bas <= sayi) {
+            if (sayi % bas == 0) {
+                System.out.print(bas + " ");
+                bolenlerAdet += 1;
+
+            }
+            bas++;
+        }
+        System.out.println("");
+        System.out.println(bolenlerAdet + " Adet boleni vardir");
+    }
+}
