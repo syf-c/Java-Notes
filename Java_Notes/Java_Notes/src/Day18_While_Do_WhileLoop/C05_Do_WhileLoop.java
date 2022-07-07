f5d742f77d5eae32141a156332177314bbadb5a8
“User <musagurbuz4661@gmail.com>
2022-07-07 12:58:24 +0200

    Do While loop

---
+++ Java_Notes/Java_Notes/src/Day18_While_Do_WhileLoop/C05_Do_WhileLoop.java
@@ -0,0 +1,33 @@
+package Day18_While_Do_WhileLoop;
+
+public class C05_Do_WhileLoop {
+    /*
+    Soru 1) 9 dan 190 a kadar olan tum tamsayilari ekrana yazdirin
+     */
+
+    public static void main(String[] args) {
+        int bas=9;
+        int son=190;
+        int temp=bas;
+
+        //while lopp
+
+        while (temp<son) {
+            if(temp%7==0){
+                System.out.print(temp+ " ");
+            }
+            temp++;
+        }
+        System.out.println("");
+        temp=bas;
+        do {
+            if(temp%7==0){
+                System.out.print(temp+ " ");
+            }
+            temp++;
+
+        }while (temp<son);
+
+    }
+
+}
