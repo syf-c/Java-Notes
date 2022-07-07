6a7d5cd2f3a65cd246cdafbf3a9a598f0b00774c
“User <musagurbuz4661@gmail.com>
2022-07-07 12:58:12 +0200

    Do While loop

---
+++ Java_Notes/Java_Notes/src/Day18_While_Do_WhileLoop/C04_Do_WhileLoop.java
@@ -0,0 +1,48 @@
+package Day18_While_Do_WhileLoop;
+
+import java.util.Scanner;
+
+public class C04_Do_WhileLoop {
+    public static void main(String[] args) {
+
+
+    /*
+    kullanicidan tam sayilar alin kullanici cift sayi girdigi muddetce sasayimari yazdirin
+    tek sayi girdiginde islemi bitirin
+     */
+
+        Scanner scan = new Scanner(System.in);
+        int sayi = 0;
+
+        //while ile yapalim
+
+        while (sayi % 2 == 0) {
+            System.out.println("Lutfen bir sayi giriniz :");
+            sayi = scan.nextInt();
+            if (sayi%2 == 0){
+                System.out.println("Girilen sayi cift "+ sayi);
+            }else{
+                System.out.println("Grilen sayi tek , benden bu kadar");
+            }
+
+        }
+
+        // do while ile yapalim
+        // do while loopda onceden olusturulan variable'a
+        // hangi deger atandiginin hic bir onemi yok kodumuz her durumda caklisir
+
+        /*
+        do while'in dezavantaji ilk calistirma kontrol yapilmadan oldugu icin loopun bodysinde yanlis
+        bir islem yapilmamasina dikkat etmek gerekir
+         */
+
+        do {
+            if (sayi%2 == 0){
+                System.out.println("Girilen sayi cift "+ sayi);
+            }else{
+                System.out.println("Grilen sayi tek , benden bu kadar");
+            }
+
+        }while(sayi%2 == 0);
+    }
+}
