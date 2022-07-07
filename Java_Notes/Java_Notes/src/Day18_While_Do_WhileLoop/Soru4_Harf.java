82abb8e5e63602be78094cf2e529f1ef93fdbe60
“User <musagurbuz4661@gmail.com>
2022-07-07 12:59:43 +0200

    Do While loop

---
+++ Java_Notes/Java_Notes/src/Day18_While_Do_WhileLoop/Soru4_Harf.java
@@ -0,0 +1,21 @@
+package Day18_While_Do_WhileLoop;
+
+public class Soru4_Harf {
+    /*
+    Soru 4 ) Kullanicidan baslangic ve bitis haflerini alin ve baslangic harfinden baslayip bitis
+    harfinde biten tum harfleri buyuk harf olarak ekrana yazdirin. Kullanicinin hata
+    yapmadigini farz edin.
+     */
+    public static void main(String[] args) {
+        char bas = 'a';
+        char bitis = 'z';
+        String temp = "";
+
+
+        while (bas <= bitis) {
+            temp += bas + " ";
+            bas++;
+        }
+        System.out.println(temp.toUpperCase());
+    }
+}
