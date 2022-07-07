72352b8ba48de54428c1341a4d6087d19d814b60
“User <musagurbuz4661@gmail.com>
2022-07-07 12:59:22 +0200

    Do While loop

---
+++ Java_Notes/Java_Notes/src/Day18_While_Do_WhileLoop/Soru2_Ortak_Bolen.java
@@ -0,0 +1,15 @@
+package Day18_While_Do_WhileLoop;
+
+public class Soru2_Ortak_Bolen {
+    /*
+    For loop ve While loop kullanarak 3 basamakli sayilardan 15,20 ve 90'a tam bolunebilen sayilari yazdirin
+     */
+    public static void main(String[] args) {
+        for (int i =100; i <=999 ; i++) {
+            while (i%15==0 &&i%20==0&&i%90==0){
+                System.out.print(i +" ");
+                i++;
+            }
+        }
+    }
+}
