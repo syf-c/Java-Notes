265cddf5182a5605924298cfeae2cba0ce98fab8
“User <musagurbuz4661@gmail.com>
2022-07-08 12:20:07 +0200

    Questin and Answer

---
+++ Question-Answer/src/QA_02.java
@@ -0,0 +1,20 @@
+public class QA_02 {
+   /* 2---1 den 20 ye kadar tam sayıları alt alta yazan Java kodu yazınız.
+    Hint: For döngüsü kullanınız.
+    Beklenen Çıktı:
+            1
+            2
+            3
+            4
+            .
+            .
+            .
+            19
+            20
+    */
+   public static void main(String[] args) {
+       for (int i =1; i <=20 ; i++) {
+           System.out.println(i);
+       }
+   }
+}
