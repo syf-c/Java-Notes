0e46ef3d535b3fc880e660b96c923f766e072e27
“User <musagurbuz4661@gmail.com>
2022-07-08 12:19:57 +0200

    Questin and Answer

--- Question-Answer/src/QA_01.java
+++ Question-Answer/src/QA_01.java
@@ -1,2 +1,23 @@
 public class QA_01 {
+    /*
+    1---Sayının Rakamlar toplaminı bulan Java kodu yazınız.
+
+    Test Data
+    34
+    Beklenen Çıktı
+    7
+     */
+    public static void main(String[] args) {
+        int sayi = 2580;
+        int rakamlarToplami=0;
+        int birlerBasamagi=0;
+
+        while(sayi>0){
+            birlerBasamagi=sayi%10;
+            rakamlarToplami +=birlerBasamagi;
+            sayi /=10;
+
+        }
+        System.out.println("Rakamlar Toplami : " + rakamlarToplami);
+    }
 }
