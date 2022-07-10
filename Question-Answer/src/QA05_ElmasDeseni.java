0ccf406f2c4ffb86eef605ff7e6e6fadb4ebab11
“User <musagurbuz4661@gmail.com>
2022-07-10 15:18:13 +0200

    elmas deseni olusturma

---
+++ Question-Answer/src/QA05_ElmasDeseni.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class QA05_ElmasDeseni {
+    /*Test Data:
+    Yarım elmas uzunluğu : 7
+    Beklenen Çıktı:
+          *
+         ***
+        *****
+       *******
+      *********
+     ***********
+    *************
+     ***********
+      *********
+       *******
+        *****
+         ***
+          *
+     */
+    public static void main(String[] args) {
+        int input = 10;
+
+        for (int i = 1; i <= input; i++) {
+            for (int j = input - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (int i = input - 1; i >= 1; i--) {
+            for (int j = input - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+
+}
+
+
