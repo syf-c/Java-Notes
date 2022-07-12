758e39fd8b5fa1a9db40e0d30bff32c2d61b395a
“User <musagurbuz4661@gmail.com>
2022-07-12 11:57:29 +0200

    écriture inversée(tersten yazma)

---
+++ Question-Answer/src/QA22_cumleyiiTers_amaDuzgun.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class QA22_cumleyiiTers_amaDuzgun {
+    /*
+    Bir String değişkeni cümlesi verildiğinde, her sözcüğü almak için kod yazın ve tersine ters çevrilmiş String'e atayın.
+
+    Test Data:
+    sentence -> "Java is fun"
+    reversed -> "fun is Java"
+     */
+
+    public static void main(String[] args) {
+        String str="Java is fun";
+        String arr[]=str.split(" ");
+        System.out.println(str);
+        for (int i = arr.length-1; i >=0 ; i--) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
