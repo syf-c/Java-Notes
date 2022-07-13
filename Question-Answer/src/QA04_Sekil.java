22a81e9ef9b8540ae171f9a9372a82a6b69e2f2b
“User <musagurbuz4661@gmail.com>
2022-07-13 14:33:19 +0200

    ucgen sekli

--- Question-Answer/src/QA04_Sekil.java
+++ Question-Answer/src/QA04_Sekil.java
@@ -17,6 +17,7 @@ public class QA04_Sekil {
   */
  public static void main(String[] args) {
      for (int i =1; i <=11 ; i++) {
+
          for (int j = 1; j <i ; j++) {
              System.out.print(j);
          }
