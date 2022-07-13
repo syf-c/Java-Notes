e083999ab979cac264008617e10d577e4744dead
“User <musagurbuz4661@gmail.com>
2022-07-13 17:59:42 +0200

    Arrays sola kaydirma

--- Java_Notes/Java_Notes/src/Day20_Arrays/C05_SolaKaydirma.java
+++ Java_Notes/Java_Notes/src/Day20_Arrays/C05_SolaKaydirma.java
@@ -13,9 +13,7 @@ public class C05_SolaKaydirma {
         int temp=arr[0];
 
         for (int i = 0; i <arr.length-1; i++){
-
             arr[i]=arr[i+1];
-
         }
         arr[arr.length-1]=temp;
         System.out.println(Arrays.toString(arr)); //[2, 3, 1]
