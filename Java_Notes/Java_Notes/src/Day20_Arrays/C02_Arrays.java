f7d65a48f496e4c478cd99186e1ada6f8409f21c
“User <musagurbuz4661@gmail.com>
2022-07-13 17:59:25 +0200

    Arrays

--- Java_Notes/Java_Notes/src/Day20_Arrays/C02_Arrays.java
+++ Java_Notes/Java_Notes/src/Day20_Arrays/C02_Arrays.java
@@ -5,32 +5,22 @@ import java.util.Arrays;
 public class C02_Arrays {
     public static void main(String[] args) {
 
-        int sayilar[] = new int[3];
-        System.out.println(sayilar); //[I@723279cf
-
+        int sayilar []=new int[3];
+        System.out.println(sayilar); // [I@58ceff1
         // non-primitive data turundeki datalari
-        //herzaman direk yazdiramayiz
-        //Array 'i yazdirmak istersek Arrays class'indan yardim isteriz
-
+        // her zaman direk yazdiramayabiliriz
+        // Array'i yazdirmak istersek Arrays class'indan yardim isteriz
         System.out.println(Arrays.toString(sayilar)); // [0, 0, 0]
-
         sayilar[2]=10;
         sayilar[0]=5;
         sayilar[1]=3;
-        System.out.println(sayilar); // [I@723279cf
-        System.out.println(Arrays.toString(sayilar)); //[5, 3, 10]
-
-        String sinifList[] = {"Ali","Veli","Ayse"};
-
-        System.out.println(Arrays.toString(sinifList)); //[Ali, Veli, Ayse]
-
+        System.out.println(sayilar); // [I@58ceff1
+        System.out.println(Arrays.toString(sayilar)); // [5, 3, 10]
+        String sinifList[]={"Ali","Ayse","Ahmet"};
+        System.out.println(Arrays.toString(sinifList)); // [Ali, Ayse, Ahmet]
         sinifList[1]="Hasan";
-        System.out.println(Arrays.toString(sinifList));
-
-        System.out.println(sinifList[1]);   // Hasan
-        System.out.println(sinifList[0]);   // Ali
-
-
-
+        System.out.println(Arrays.toString(sinifList)); // [Ali, Hasan, Ahmet]
+        System.out.println(sinifList[1]); // hasan'i yazdiralim
+        System.out.println(sinifList[0]); // Ali
     }
 }
