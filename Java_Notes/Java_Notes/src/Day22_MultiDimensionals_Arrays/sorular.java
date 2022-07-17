fd88193eb7dd33c48734faa55203847c907f2e26
“User <musagurbuz4661@gmail.com>
2022-07-17 23:34:38 +0200

    MultiDimensionals_Arrays

---
+++ Java_Notes/Java_Notes/src/Day22_MultiDimensionals_Arrays/sorular.java
@@ -0,0 +1,25 @@
+package Day22_MultiDimensionals_Arrays;
+
+public class sorular {
+    /*
+      Soru 1 ) Asagidaki multi dimensional array'in tum elemanlarinin carpimini ekrana
+        yazdiran bir method yaziniz. { {1,2,3), {4,5,6} }
+
+      Soru 2) Asagidaki multi dimensional array'in ic array'lerindeki son elemanlarin carpimini
+        ekrana yazdiran bir program yaziniz { {1,2,3}, (4,5), (6} }
+
+      Soru 3) Asagidaki multi dimensional array'lerin ic array'lerinde ayni index'e sahip
+             elemanlarin toplamini ekrana yazdiran bir program yaziniz. (Zor soru) arr1 = { {1,2},
+             [3,4,5), {6} } ve arr2 = { {7,8,9}, {10,11}, (12} }
+      Soru 4) Asagidaki multi dimensional array'in ic array'lerindeki tum elemanlarin toplamini
+              birer birer bulan ve herbir sonucu yeni bir array'in elemani yapan ve yeni array'i ekrana
+              yazdiran bir program yaziniz { {1,2,3), {4,5), {6,7} }
+                Ornek; { {1,2,3), {4,5), (6,7} } ==> 1+2+3=6 4+5=9 6+7=13 ==> output: {6, 9, 13}
+      Soru 5) Kullanicidan bir cumle isteyin ve cumledeki kelime sayisini yazdirin
+
+      Soru 6) Verilen bir Array'den isten degere esit olan elamanlari kaldirip, kalanlari yeni bir
+              Array olarak yazdiran bir method yaziniz
+
+
+     */
+}
