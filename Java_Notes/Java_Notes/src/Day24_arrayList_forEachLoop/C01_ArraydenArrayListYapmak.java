e73448982314714dad9006703a38de32efef4fa3
“User <musagurbuz4661@gmail.com>
2022-07-19 11:46:19 +0200

    Arrays.asList

---
+++ Java_Notes/Java_Notes/src/Day24_arrayList_forEachLoop/C01_ArraydenArrayListYapmak.java
@@ -0,0 +1,42 @@
+package Day24_arrayList_forEachLoop;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class C01_ArraydenArrayListYapmak {
+    public static void main(String[] args) {
+
+
+        String[] arr = {"ismail", "nurrullah", "hasan"};
+      /*
+        Uzun listeler olusturmamiz gerektiginde,
+        tek tek eklemek yerine array olusturup, bunlari list'e cevirmek
+        daha pratik olabilir
+        1- loop ile array'deki tum elementleri list'e atabiliriz
+        2- Arrays.asList() kullanabiliriz
+           Ancak bu method'un 2 tane kotu yan etkisi var
+           - Arrays class'i kullanildigi icin array ozellikleri gecerli olur
+             dolayisiyla list'te olan add, remove gibi size'i degistiren method'lar
+             bu sekilde olusturulan list'lerde kullanilamaz
+           - kaynak olan array ile urun olan list ozdeslestirilir
+             birinde yapilan degisiklik, otomatik olarak digerine de islenir
+         */
+        List<String> sinifList = Arrays.asList(arr);
+        System.out.println(sinifList);//[ismail, nurrullah, hasan]
+
+        //1.yan etki
+      //  sinifList.add("Erdi");     //UnsupportedOperationException
+      //  sinifList.remove(1); //UnsupportedOperationException
+
+        arr[1]= "Emre";
+        System.out.println("degisim sonrasi array : "+Arrays.toString(arr));
+        System.out.println("array'i degistirince list : "+sinifList);
+
+        sinifList.set(0,"Utku");
+        System.out.println("Listi degistirince list : "+sinifList);
+        System.out.println("listi degistirince Array : "+ Arrays.toString(arr));
+
+
+
+    }
+}
