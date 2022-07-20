cdc3a354390b7889bd341e27a8abb04a097d32b4
“User <musagurbuz4661@gmail.com>
2022-07-20 21:45:19 +0200

    Constructor

--- Java_Notes/Java_Notes/src/Day25_constructor/Car.java
+++ Java_Notes/Java_Notes/src/Day25_constructor/Car.java
@@ -2,23 +2,23 @@ package Day25_constructor;
 
 public class Car {
     /*
-    Bu class vizom kaliphanemiz
-    bir araba olusturmak icin ihtiyacimiz olan
-    variable ve methodlari bu class'da belirleriz
+   Bu class bizim kaliphanemiz
+   bir araba olusturmak icin ihtiyacimiz olan
+   variable ve method'lari bu class'da belirleriz
+   sonra farkli class'larda araba olusturmamiz gerekirse
+   bu class'dan bir obje olusturup
+   burada belirlenen variable ve method'lara gore araba uretiri
+    */
+    public String marka = "Marka belirtilmedi";
+    public String model = "Model belirtilmedi";
+    public int yil;
+    public int fiyat;
 
-    sonra farkli class 'larda araba olusturmamiz gerekirse
-    bu class' dan bir obje olusturup
-    burada belirlenen variable ve method'lara gore araba uretir
-     */
-    String marka ="Marka belirtilmedi";
-    String model= "Model belirtilmedi";
-    int yil;
-    int fiyat;
-
-    public void benzinliArac(){
+    public void benzinliArac() {
         System.out.println("Bu arac benzinli motor sahiptir");
     }
-    public void maxHiz(int hiz){
-        System.out.println("Bu araba max "+ hiz +" km hiz yapar");
+
+    public void maxHiz(int hiz) {
+        System.out.println("Bu araba max " + hiz + " km hiz yapar");
     }
 }
