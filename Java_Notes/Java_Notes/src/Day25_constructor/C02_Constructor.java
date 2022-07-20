62e6012f7861859aedc76e16c9cfa2a30aceb086
“User <musagurbuz4661@gmail.com>
2022-07-20 21:44:44 +0200

    Constructor

--- Java_Notes/Java_Notes/src/Day25_constructor/C02_Constructor.java
+++ Java_Notes/Java_Notes/src/Day25_constructor/C02_Constructor.java
@@ -7,6 +7,16 @@ public class C02_Constructor {
         // class adi     obje adi            keyword      constructor
         System.out.println(obj1.sayi); // 0
         obj1.deneme();//C01'den deneme method calisir
+       // Scanner scan = new Scanner(System.in);
+       // Random rnd= new Random();
+      //  List<String> list= new ArrayList<>();
+
+        /*
+        Javada bir obje olusturabilmek icin
+        mutlaka constructor kullanmalisiniz
+         */
+        System.out.println(obj1.sayi); // 0
+        obj1.deneme(); // C01 den deneme method calisir
 
         //constructor= obje olusturmak icin kullanilan kod blogudur
         //
