16751f708ad0687678130167085ff4eb75570526
“User <musagurbuz4661@gmail.com>
2022-07-20 21:43:39 +0200

    Constructor

---
+++ Java_Notes/Java_Notes/src/Day26_constructor/Araba_Tekrar.java
@@ -0,0 +1,27 @@
+package Day26_constructor;
+
+public class Araba_Tekrar {
+
+    public String marka="Herhangi bir Marka atanmadi";
+    public String model="Herhangi bir Model atanmadi";
+    public int yil;
+    public int fiyat;
+
+    public Araba_Tekrar(String markaR, String modelR, int yilR, int fiyatR) {
+        marka=markaR;
+        model=modelR;
+        yil=yilR;
+        fiyat=fiyatR;
+    }
+
+    public Araba_Tekrar() {
+    }
+
+    public Araba_Tekrar(String marka, String model, int yil) {
+        this.marka = marka;
+        this.model = model;
+        this.yil = yil;
+    }
+
+
+}
