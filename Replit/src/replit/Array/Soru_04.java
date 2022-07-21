cdbbc5488eaa2eb73a5ae722401fd35f986160ee
“User <musagurbuz4661@gmail.com>
2022-07-21 20:43:05 +0200

    Array

---
+++ Replit/src/replit/Array/Soru_04.java
@@ -0,0 +1,13 @@
+package replit.Array;
+
+import java.util.Arrays;
+
+public class Soru_04 {
+
+    public static void main(String[] args) {
+        String input1 = "John";
+        String[] arr1 = input1.split("");
+        System.out.println(Arrays.toString(arr1));
+    }
+}
+
