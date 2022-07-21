f240e7da67ec84f6798bc756e3f798994060d8eb
“User <musagurbuz4661@gmail.com>
2022-07-21 20:42:41 +0200

    Array

---
+++ Replit/src/replit/List/Soru_01_enFazlaOlanKarakter.java
@@ -0,0 +1,36 @@
+package replit.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Soru_01_enFazlaOlanKarakter {
+    /*
+    Write a Java program to get a String from user as input and find the maximum occurring character in that string. (Ignore case sensitivity)
+
+    input :
+    Learning java is easy
+    output:
+    maximum occurring character is : a
+     */
+    public static void main(String[] args) {
+        String input = "Learning java is easy";
+        input = input.replaceAll(" ", "");
+        String[] arr = input.split("");
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        List<String> repeated = new ArrayList<String>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i].equalsIgnoreCase(arr[j]) && !repeated.contains(arr[i])) {
+                    repeated.add(arr[i]);
+                }
+            }
+        }
+        System.out.println(repeated);
+
+    }
+}
