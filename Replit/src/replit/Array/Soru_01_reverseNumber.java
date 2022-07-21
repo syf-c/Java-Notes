30c4e17901a337039135d60c1a45b802e3bc0c84
“User <musagurbuz4661@gmail.com>
2022-07-21 20:42:49 +0200

    Array

---
+++ Replit/src/replit/Array/Soru_01_reverseNumber.java
@@ -0,0 +1,33 @@
+package replit.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Soru_01_reverseNumber {
+    /*
+    Write a return method to reverse number.
+
+    Input : 12345
+    Output : 54321
+     */
+    public static void main(String[] args) {
+        int input = 12345;
+        reverseNumber(input);
+
+    }
+
+    private static void reverseNumber(int input) {
+        int birlerBas = 0;
+
+        String container = "";
+        while (input != 0) {
+            birlerBas = input % 10;
+
+            container = birlerBas + "";
+            input /= 10;
+            System.out.print(container);
+        }
+    }
+}
+
