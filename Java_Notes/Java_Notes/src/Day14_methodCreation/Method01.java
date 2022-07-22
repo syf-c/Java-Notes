e4be237f0c82ef8f84fde1e2f600df9c442818bf
“User <musagurbuz4661@gmail.com>
2022-07-22 20:43:40 +0200

    testen yazma

--- Java_Notes/Java_Notes/src/Day14_methodCreation/Method01.java
+++ Java_Notes/Java_Notes/src/Day14_methodCreation/Method01.java
@@ -6,13 +6,14 @@ public class Method01 {
     //   int sonuc = toplama(1,2,3);
      //   System.out.println(sonuc);
         System.out.println("sayilarin toplami : "+toplama(1,2,3));
+        toplama(1,2,3);
     }
 
-    public static int toplama(int a,int b,int c) {
+    public static int toplama(int a, int b, int c) {
 
-       // System.out.println("toplamlari : "+(a+b+c));
+      System.out.println("toplamlari : "+(a+b+c));
 
-        return   a+b+c;
+       return   a+b+c;
     }
 
 
