e01e97f320ab3b288cb9c2b9e0e58e5268466158
“User <musagurbuz4661@gmail.com>
2022-07-27 10:17:15 +0200

    obeb okek

---
+++ Replit/src/replit/For_While/Obeb_Okek.java
@@ -0,0 +1,30 @@
+package replit.For_While;
+
+import java.util.Scanner;
+
+public class Obeb_Okek {
+    public static void main(String[] args) {
+        /*
+        Kullanıcıdan 2 tamsayı girmesini ve ardından GCD (En Büyük Ortak Bölen) ve LCM'yi (En Küçük Ortak Kat) bulmasını isteyin
+
+        Giriş :
+
+        30 ve 40
+
+        Beklenen çıktı:
+
+        30 ve 40 = 10 için OBEB
+
+        30 ve 40 = 120 için OKEK
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("lutfen birinci tam sayiyi giriniz : ");
+        int sayi1 = scan.nextInt();
+        System.out.print("lutfen ikinci tamsayiyi giriniz : ");
+        int sayi2 = scan.nextInt();
+        int sayac = 1;
+
+
+    }
+}
