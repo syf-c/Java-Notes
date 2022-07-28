18da47a398ab70abd2eba4dd73b85a4656250c80
“User <musagurbuz4661@gmail.com>
2022-07-28 22:05:29 +0200

    inheritance

---
+++ Java_Notes/Java_Notes/src/Day34_inheritance/ToyotaCorolla.java
@@ -0,0 +1,22 @@
+package Day34_inheritance;
+
+public class ToyotaCorolla extends Toyato {
+    public static void main(String[] args) {
+        /*
+        child class'dan parent class'a erisimde
+        access modifier kurallarini bypass edemeyiz
+        ornegin parent class 'daki private class uyelerini
+        child class'dan kullanamayiz
+
+        ayni sekilde parent ve child farkli package larda ise
+        parent class'daki default access modifier'i olan
+        class uyelerini child class'dan kullanamayiz
+         */
+
+        ToyotaCorolla arb1 = new ToyotaCorolla();
+
+        System.out.println(arb1.marka);// Toyota
+        System.out.println(arb1.model);// Model belirtilmedi
+
+    }
+}
