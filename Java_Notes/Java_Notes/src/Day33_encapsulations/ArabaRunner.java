c590bb347734abbd72bea7cc1305ce7cba95008b
“User <musagurbuz4661@gmail.com>
2022-07-28 22:05:09 +0200

    inheritance

---
+++ Java_Notes/Java_Notes/src/Day33_encapsulations/ArabaRunner.java
@@ -0,0 +1,9 @@
+package Day33_encapsulations;
+
+public class ArabaRunner {
+    public static void main(String[] args) {
+        Araba araba = new Araba();
+        araba.marka = "Toyota";
+
+    }
+}
