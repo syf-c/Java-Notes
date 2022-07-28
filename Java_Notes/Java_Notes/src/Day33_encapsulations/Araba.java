1ab47596e951ae7770b249c187e1ef030dec7c76
“User <musagurbuz4661@gmail.com>
2022-07-28 22:07:12 +0200

    encapsulations

--- Java_Notes/Java_Notes/src/Day33_encapsulations/Araba.java
+++ Java_Notes/Java_Notes/src/Day33_encapsulations/Araba.java
@@ -1,5 +1,18 @@
 package Day33_encapsulations;
 
 public class Araba {
-    public String marka;
+    String marka="Marka belirtilmedi"; // marka'nin access modifier'i default access modifier
+    // oldugundan, package icerisinde kullanilabilir
+    private String model="Model belirtilmedi";
+    private String yakit="Elektrikli"; // tum arabalar elektrikli ise bu variable'in degistirilmemesi lazim
+    // private yaptigimiz model ve yakit variable'larina erisimi yetkilendirelim
+    // model'e deger atanabilsin ama gorulemesin(setter)
+    // yakit ise gorulebilsin ama yeni deger atanamasin isteniyor (getter)
+    //
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getYakit() {
+        return yakit;
+    }
 }
