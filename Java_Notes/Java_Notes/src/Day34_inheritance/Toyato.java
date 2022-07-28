512cc270fb9b7756036b8f75a5f320dd82b91c4a
“User <musagurbuz4661@gmail.com>
2022-07-28 22:05:25 +0200

    inheritance

---
+++ Java_Notes/Java_Notes/src/Day34_inheritance/Toyato.java
@@ -0,0 +1,19 @@
+package Day34_inheritance;
+
+public class Toyato {
+
+    protected String marka ="Toyota";
+    protected String model = "Model belirtilmedi";
+    protected String yakit= "yakit belirtilmedi";
+
+    protected void motor(){
+        System.out.println("Toyota cevreci motorlar kullanir");
+    }
+
+    protected void akut(){
+        System.out.println("Toyota modele gore aku kullanir");
+    }
+
+
+
+}
