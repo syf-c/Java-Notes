5dd159f5433872248228834add41520ab6ae402c
“User <musagurbuz4661@gmail.com>
2022-07-29 17:42:36 +0200

    encapsulations

---
+++ Java_Notes/Java_Notes/src/Day33_encapsulations/OgretmenRunner.java
@@ -0,0 +1,15 @@
+package Day33_encapsulations;
+
+public class OgretmenRunner {
+    public static void main(String[] args) {
+
+        Ogretmen ogr1 = new Ogretmen();
+
+        ogr1.setIsim("Tulay");
+        System.out.println(ogr1.getIsim());
+         /*
+        Bu yontemde data hiding degil
+        daha anlasilir bir kod amaclanmis olur
+         */
+    }
+}
