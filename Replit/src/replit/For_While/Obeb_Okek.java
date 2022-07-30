5841ff832c0603f331790f1d4c6385fa6d9c1f72
“User <musagurbuz4661@gmail.com>
2022-07-30 09:58:58 +0200

    obeb okek

--- Replit/src/replit/For_While/Obeb_Okek.java
+++ Replit/src/replit/For_While/Obeb_Okek.java
@@ -18,12 +18,30 @@ public class Obeb_Okek {
         30 ve 40 = 120 için OKEK
          */
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("lutfen birinci tam sayiyi giriniz : ");
-        int sayi1 = scan.nextInt();
-        System.out.print("lutfen ikinci tamsayiyi giriniz : ");
-        int sayi2 = scan.nextInt();
-        int sayac = 1;
+        /* FOR WHILE 2. SORU
+       Ask user to enter 2 integer then find GCD (Greatest Common Divisor) and LCM (Least Common Multiple)
+        Input :
+        30 and 40
+        Expected OutPut:
+        GCD for 30 and 40 = 10
+        LCM for 30 and 40 = 120 */
+        int a = 30;
+        int b = 40;
+        int ebob = 1;
+        if (a == 0 || b == 0) {
+            System.out.println("Sifir'in EBOB/EKOK'u yoktur");
+        } else if (a < 0 || b < 0) {
+            System.out.println("Lutfen 0'dan buyuk iki sayi giriniz");
+        } else {
+            for (int i = 1; i <= 30 && i <= 40; i++) {
+                if (a % i == 0 && b % i == 0) {
+                    ebob = i;
+                }
+            }
+            System.out.println("GCD for 30 and 40 = " + ebob);   //  EBOB 10
+            int ekok = (a * b) / ebob;    // FORMUL = a * b = EBOB(a,b) * EKOK(a,b)
+            System.out.println("LCM for 30 and 40 = " + ekok);  // EKOK 120
+        }
 
 
     }
