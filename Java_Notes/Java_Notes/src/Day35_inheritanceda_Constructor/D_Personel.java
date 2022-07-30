a60ae43cd8b6f38b069a614204450c4bda677e8b
“User <musagurbuz4661@gmail.com>
2022-07-30 09:57:31 +0200

    Using Constructor in inheritance

---
+++ Java_Notes/Java_Notes/src/Day35_inheritanceda_Constructor/D_Personel.java
@@ -0,0 +1,11 @@
+package Day35_inheritanceda_Constructor;
+
+public class D_Personel {
+
+    D_Personel(){
+        System.out.println("D_personel parametresiz constructor");
+    }
+    D_Personel(String isim){
+        System.out.println("D_personel parametreli constructor");
+    }
+}
