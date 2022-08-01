708702221e89c3efd09b42195597c1b1ed7cbf89
“User <musagurbuz4661@gmail.com>
2022-08-01 22:23:52 +0200

    Day37_overridding

---
+++ Java_Notes/Java_Notes/src/Day37_overridding/D_Araba.java
@@ -0,0 +1,15 @@
+package Day37_overridding;
+
+public class D_Araba {
+
+    void yakit(){
+        System.out.println("Tum arabalar yakit kullanir");
+    }
+    void marka(){
+        System.out.println("Tum arabalarin markasi vardir");
+    }
+
+    void motor(){
+        System.out.println("Tum arabalarin mootoru vardir");
+    }
+}
---
+++ Java_Notes/Java_Notes/src/Day37_overridding/E_Toyota.java
@@ -0,0 +1,12 @@
+package Day37_overridding;
+
+public class E_Toyota extends D_Araba {
+    void marka(){
+        System.out.println("Markamiz Toyota");
+    }
+
+
+    void motor() {
+        System.out.println("Toyota araclar Toyota marka motor kullanir");
+    }
+}
