5a56305cf1420cf568b48d6989814f02e8d705a1
“User <musagurbuz4661@gmail.com>
2022-08-01 22:23:23 +0200

    _overloadding

---
+++ Java_Notes/Java_Notes/src/Day37_overridding/C01_overloadding.java
@@ -0,0 +1,34 @@
+package Day37_overridding;
+
+public class C01_overloadding {
+
+    public static void main(String[] args) {
+        /*
+        Bir class'da ayni isimde ve ayni signature'a sahip iki method olmaz
+        Ayni class' da ayni isimde birden fazla method olusturmak istersek
+        mutlaka signature'i degistirmeliyiz
+
+        Farkli class larda ayni isim ve signature/a sahip iki method olabilir mi ? tabiki ewet
+*/
+
+
+    }
+    void ekleme(){
+
+    }
+    void ekleme(int sayi){
+
+    }
+    void ekleme(String str){
+
+    }
+    void ekleme(int sayi, String str){
+
+    }
+    void ekleme(String str,int sayi){
+
+    }
+
+
+
+}
