a3efbcb952720f0e2b012db9b0de6cb95270c94b
“User <musagurbuz4661@gmail.com>
2022-08-01 19:52:45 +0200

    Soru06_Alfabet

---
+++ Replit/src/replit/For_While/Soru06_Alfabet.java
@@ -0,0 +1,14 @@
+package replit.For_While;
+
+public class Soru06_Alfabet {
+    //Print Alphabet on the console.
+    public static void main(String[] args) {
+        char baslangic='a';
+        char son = 'z';
+        for (int i = baslangic; i <=son;i++){
+            System.out.print(baslangic+" ");
+            baslangic++;
+        }
+    }
+    }
+
