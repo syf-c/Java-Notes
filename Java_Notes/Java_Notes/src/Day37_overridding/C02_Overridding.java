12ae0cb8c1bb7304e613d954f612787a7e5990a5
“User <musagurbuz4661@gmail.com>
2022-08-01 22:23:34 +0200

    Day37_overridding

---
+++ Java_Notes/Java_Notes/src/Day37_overridding/C02_Overridding.java
@@ -0,0 +1,21 @@
+package Day37_overridding;
+
+public class C02_Overridding {
+    public static void main(String[] args) {
+        /*
+    Farkli class lardaki ayni isim ve signature'da
+    method' lar olusturabiliriz
+    cunku her bir class kendi icinde calisir
+
+    */
+    }
+    void ekleme(){
+
+    }
+    void ekleme(int sayi){
+
+    }
+    void ekleme(String str){
+
+    }
+}
