32219139c1046763d8a5ba86ffa6377d85428bae
“User <musagurbuz4661@gmail.com>
2022-08-02 21:22:51 +0200

    Okul_Project

---
+++ Practice question days/src/Day12_Okul_Project/Runner.java
@@ -0,0 +1,9 @@
+package Day12_Okul_Project;
+
+import static Day12_Okul_Project.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+       girisPaneli();
+    }
+}
