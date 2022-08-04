55a2ea127ba55d85e6b3177a30e6e1ed0d31ad59
“User <musagurbuz4661@gmail.com>
2022-08-04 11:07:36 +0200

    Exceptions

--- Java_Notes/Java_Notes/src/Day38_Exception/C03_Exceptions.java
+++ Java_Notes/Java_Notes/src/Day38_Exception/C03_Exceptions.java
@@ -1,25 +1,40 @@
 package Day38_Exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class C03_Exceptions {
     public static void main(String[] args) {
-        String str= "123a5";
-        int sayi= 0;
-        try {
-            sayi = Integer.parseInt(str);
-        } catch (NumberFormatException e) {
-            System.out.println("Yazdiginiz deger sadece rakamlardan olusmalidir");
-        }
-        catch (Exception e) {
-            System.out.println("Ongorulemeyen bir hata olustu");
-        }
-        System.out.println("Sayinin karesi : " + sayi*sayi);
         /*
-          Kullanicidan String olarak bir deger aliyor
-          ve bunu integer'a ceviriyorsak
-          NumberFormatException ile karsilasabilecegimizi ongoruruz
-          NFE aldigimizda kodun durmamasini istiyorsak
-          try catch ile cevreleyebiliriz
-          Eger bilmedigim bir exception daha olusursa
-          kodum durmasin istiyorsak
-          bir kere daha catch cumlesi ekleyip ona en genis Exception'i yazabiliriz.
+          Marketteki tum urunleri bir array'de tuttugumuzu varsayalim
+          Kullaniciya index sorup, o index'deki urunu yazdiran bir
+          program hazirlayalim
+          Kullanici urun sayisindan buyuk bir index girerse
+          exception vermesinin onune gecelim
          */
+        String[] urunler={"Nutella","Cokokrem","Sut","Cay","Findik"};
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Istediginiz urunun sira nosunu giriniz");
+        int istenenSira=0;
+        try {
+            istenenSira =scan.nextInt();
+        } catch (InputMismatchException deneme) {
+            System.out.println("Urun indexi icin bir tam sayi girmeniz gerekli");
+            System.out.println(deneme);
+        }
+        /*
+         catch blogunun onundeki parantezde
+         exception class'inin ismi ve yaninda
+         yakalanan exception'i atadigimiz variable'in ismi olur (e)
+         Eger yakalanan exception ile ilgili bilgileri
+         kullaniciya vermek isterseniz
+         bu objeyi kullanabilirsiniz
+         eger exception ile ilgili kullaniciya bilgi verme ihtiyaci yoksa
+         e kullanilmasa da kod calisir
+         */
+        try {
+            System.out.println("Aradiginiz urun : " + urunler[istenenSira-1] );
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Girdiginiz sira listemizde bulunmuyor" +
+                    "\nSira numarasi en fazla : " + urunler.length  + " olabilir" );
+        }
     }}
