aa61a6418ca94e30b4aebe2609fe7947fcc3ee1c
“User <musagurbuz4661@gmail.com>
2022-08-09 20:18:54 +0200

    using List Iterator

---
+++ Java_Notes/Java_Notes/src/Day44_Collections/C01_ListIterator.java
@@ -0,0 +1,35 @@
+package Day44_Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class C01_ListIterator {
+    public static void main(String[] args) {
+        /*
+        Soru 3) Bir listedeki istenen sayi araliginda olmayan elementleri silen bir program yaziniz …                                         (2. liste olusturmadan, gecerli liste uzerinde islem yapiniz)
+        Orn : [2,13,56,23,45,14,40] istenen aralik 20 ile 40 arasi (sinirlar dahil)
+        output: [23,40]
+         */
+        List<Integer> liste =new ArrayList<>();
+        liste.add(2);
+        liste.add(13);
+        liste.add(56);
+        liste.add(23);
+        liste.add(45);
+        liste.add(14);
+        liste.add(40);
+
+        ListIterator li1 = liste.listIterator();
+        int baslangic = 20;
+        int bitis = 40;
+
+        while (li1.hasNext()){
+            int temp = (Integer) li1.next();
+            if (temp<baslangic || temp>bitis){
+                li1.remove();
+            }
+        }
+        System.out.println(liste);
+    }
+}
