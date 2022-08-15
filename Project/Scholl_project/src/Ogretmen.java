c09f6210016b03b4ffaf6142f1d767b2c4a8f5a1
“User <musagurbuz4661@gmail.com>
2022-08-15 20:16:28 +0200

    school_project

---
+++ Project/Scholl_project/src/Ogretmen.java
@@ -0,0 +1,37 @@
+public class Ogretmen extends KisiOrtakBilgileri{
+    private String bolum;
+    private String sicilNo;
+
+    public Ogretmen(String adSoyad, String kimlikNo, int yas, String bolum, String sicilNo) {
+        super(adSoyad, kimlikNo, yas);
+        this.bolum = bolum;
+        this.sicilNo = sicilNo;
+    }
+
+    public Ogretmen() {
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getSicilNo() {
+        return sicilNo;
+    }
+
+    public void setSicilNo(String sicilNo) {
+        this.sicilNo = sicilNo;
+    }
+
+    @Override
+    public String toString() {
+        return  "OGRETMEN BILGILERI"
+                +super.toString()+
+                "\nBolum    = " + bolum +
+                "\nSicil No = " + sicilNo ;
+    }
+}
