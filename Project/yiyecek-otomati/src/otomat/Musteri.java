4388dfea5fba8cfb0485005143b5e3d8521c83b4
“User <musagurbuz4661@gmail.com>
2022-08-15 14:40:57 +0200

    Automat

---
+++ Project/yiyecek-otomati/src/otomat/Musteri.java
@@ -0,0 +1,13 @@
+package otomat;
+
+public class Musteri {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("===================================");
+        System.out.println("==== Welcome to Yiyecek Otomat ====");
+        System.out.println("===================================");
+        Urunler.urunlisteleme();
+        Urunler.urunSec();
+
+
+    }
+}
