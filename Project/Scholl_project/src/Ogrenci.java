a22ddbf1364381f6b2d6d21209c9a19c1ebf3a19
“User <musagurbuz4661@gmail.com>
2022-08-15 20:16:21 +0200

    school_project

---
+++ Project/Scholl_project/src/Ogrenci.java
@@ -0,0 +1,39 @@
+public class Ogrenci extends KisiOrtakBilgileri {
+
+    private String ogrenciNo;
+    private String sinif;
+
+    public Ogrenci(String adSoyad, String kimlikNo, int yas, String ogrenciNo, String sinif) {
+        super(adSoyad, kimlikNo, yas);
+        this.ogrenciNo = ogrenciNo;
+        this.sinif = sinif;
+    }
+
+
+    public Ogrenci() {
+    }
+
+    public String getOgrenciNo() {
+        return ogrenciNo;
+    }
+
+    public void setOgrenciNo(String ogrenciNo) {
+        this.ogrenciNo = ogrenciNo;
+    }
+
+    public String getSinif() {
+        return sinif;
+    }
+
+    public void setSinif(String sinif) {
+        this.sinif = sinif;
+    }
+
+    @Override
+    public String toString() {
+        return  "OGRENCI BILGILERI"
+                +super.toString()+
+                "\nOgrenci No   = " + ogrenciNo +
+                "\nSinif        = " + sinif ;
+    }
+}
