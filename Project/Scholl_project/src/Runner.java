96679874b702089ed4712b9d5f51c3cbdfd658fa
“User <musagurbuz4661@gmail.com>
2022-08-16 06:36:54 +0200

    school_project

--- Project/Scholl_project/src/Runner.java
+++ Project/Scholl_project/src/Runner.java
@@ -1,5 +1,5 @@
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Islemler.girisPaneli();
     }
 }
