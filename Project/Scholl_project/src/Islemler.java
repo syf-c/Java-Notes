5175972eefb2135d25af1313a397fb4ed656d962
“User <musagurbuz4661@gmail.com>
2022-08-16 12:26:37 +0200

    school_project

--- Project/Scholl_project/src/Islemler.java
+++ Project/Scholl_project/src/Islemler.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,6 +9,24 @@ public class Islemler {
     static Scanner scan = new Scanner(System.in);
     static String kisiTuru;
     public static Thread Tread;
+
+    public static void hazirListe(){
+        Ogrenci ogr1 = new Ogrenci("Musa Gurbuz","123",25,"10","10A");
+        ogrenciList.add(ogr1);
+        Ogrenci ogr2 = new Ogrenci("Mualla Gurbuz","456",23,"11","10A");
+        ogrenciList.add(ogr2);
+        Ogrenci ogr3 = new Ogrenci("Sule Gurbuz","789",24,"12","10A");
+        ogrenciList.add(ogr3);
+
+
+        Ogretmen ogrt1 = new Ogretmen("Musa Gurbuz","123",25,"Bilgisayar","123");
+        ogretmenList.add(ogrt1);
+        Ogretmen ogrt2 = new Ogretmen("Sule Gurbuz","456",24,"Teknoloji","456");
+        ogretmenList.add(ogrt2);
+        Ogretmen ogrt3 = new Ogretmen("Mualla Gurbuz","789",23,"Tip","789");
+        ogretmenList.add(ogrt3);
+
+    }
 
     public static void girisPaneli() throws InterruptedException {
         System.out.println(Renklendirme.ANSI_GREEN + "====================================" + Renklendirme.ANSI_RESET);
@@ -44,13 +63,14 @@ public class Islemler {
 
     }
 
-    private static void cikis() {
+
+    public static void cikis() {
         System.out.println(Renklendirme.ANSI_BLACK +Renklendirme.ANSI_YELLOW_BACKGROUND+ "=================================================" + Renklendirme.ANSI_RESET);
         System.out.println(Renklendirme.ANSI_BLACK +Renklendirme.ANSI_YELLOW_BACKGROUND+ "=  Cikisiniz yapilmistir iyi gunler dileriz :)  =" + Renklendirme.ANSI_RESET);
         System.out.println(Renklendirme.ANSI_BLACK +Renklendirme.ANSI_YELLOW_BACKGROUND+ "=================================================" + Renklendirme.ANSI_RESET);
     }
 
-    private static void islemMenusu() throws InterruptedException {
+    public static void islemMenusu() throws InterruptedException {
         System.out.println(Renklendirme.ANSI_GREEN_BACKGROUND + Renklendirme.ANSI_BLACK + "=============  " + kisiTuru + "    ===========" + Renklendirme.ANSI_RESET);
         System.out.println("=============  ISLEMLERI  ===========\n" +
                 "      1-EKLEME\n" +
@@ -85,15 +105,16 @@ public class Islemler {
                 cikis();
                 break;
             default:
-                System.out.println(Renklendirme.ANSI_RED + "       Gecerli bir bolum seciniz    " + Renklendirme.ANSI_RESET);
-                System.out.println();
+                System.out.println(Renklendirme.ANSI_YELLOW_BACKGROUND + Renklendirme.ANSI_BLACK + "        Hatali giris yaptiniz       " + Renklendirme.ANSI_RESET);
+                Tread.sleep(3000);
+
                 islemMenusu();
                 break;
 
         }
     }
 
-    private static void silme() throws InterruptedException {
+    public static void silme() throws InterruptedException {
         System.out.println(Renklendirme.ANSI_GREEN_BACKGROUND + Renklendirme.ANSI_BLACK + "=============  " + kisiTuru + "    ===========" + Renklendirme.ANSI_RESET);
         boolean flag = true;
         if (kisiTuru.equalsIgnoreCase("OGRENCI")) {
@@ -117,7 +138,7 @@ public class Islemler {
         }
     }
 
-    private static void arama() throws InterruptedException {
+    public static void arama() throws InterruptedException {
         System.out.println(Renklendirme.ANSI_GREEN_BACKGROUND + Renklendirme.ANSI_BLACK + "=============  " + kisiTuru + "    ===========" + Renklendirme.ANSI_RESET);
         boolean flag = true;
         if (kisiTuru.equalsIgnoreCase("OGRENCI")) {
@@ -158,7 +179,7 @@ public class Islemler {
         }
     }
 
-    private static void listeleme() throws InterruptedException {
+    public static void listeleme() throws InterruptedException {
         System.out.println(Renklendirme.ANSI_GREEN_BACKGROUND + Renklendirme.ANSI_BLACK + "=============  " + kisiTuru + "    ===========" + Renklendirme.ANSI_RESET);
         if (kisiTuru.equalsIgnoreCase("OGRENCI")) {
             System.out.println(ogrenciList);
@@ -179,8 +200,19 @@ public class Islemler {
         System.out.println("Kimlik numarasi giriniz :");
         String kimlikNo = scan.nextLine().replaceAll(" ", "");
 
-        System.out.println("Yas giriniz :");
-        int yas = scan.nextInt();
+
+        int yas = 0;
+        boolean kontrol = true;
+        while (kontrol){
+        try {
+            scan = new Scanner(System.in);
+            System.out.println("Yas giriniz :");
+            yas = scan.nextInt();
+            kontrol=false;
+        } catch (InputMismatchException e) {
+            System.out.println("Yasinizi tekrar giriniz");
+
+        }}
 
         if (kisiTuru.equals("OGRENCI")) {
 
