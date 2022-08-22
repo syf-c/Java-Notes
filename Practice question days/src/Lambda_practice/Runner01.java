a6902c087f4b85b55df342ed7922310c0f2568d0
“User <musagurbuz4661@gmail.com>
2022-08-22 17:14:59 +0200

    Lambda

---
+++ Practice question days/src/Lambda_practice/Runner01.java
@@ -0,0 +1,15 @@
+package Lambda_practice;
+
+public class Runner01 extends Lambda01 {
+    public static void main(String[] args) {
+        AHarfiOlanlariSil(names);
+        System.out.println("**************************");
+        System.out.println("Enbuyuk Eleman : "+enBuyukEleman(numbers));
+        System.out.println("**************************");
+        System.out.println("Tum Elemanlarin Toplami : "+tumElemanlarinToplami(numbers));
+        System.out.println("**************************");
+        System.out.println("**************************");
+        System.out.println("**************************");
+
+    }
+}
