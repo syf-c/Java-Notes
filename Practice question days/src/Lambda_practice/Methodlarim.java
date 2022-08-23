9534dca1f3be10381c526df511b92b67ded6bba1
“User <musagurbuz4661@gmail.com>
2022-08-23 17:15:22 +0200

    Lambda Expression

--- Practice question days/src/Lambda_practice/Methodlarim.java
+++ Practice question days/src/Lambda_practice/Methodlarim.java
@@ -5,4 +5,8 @@ public class Methodlarim { //seed(tohum) methodlar create edildi
     return x%2==0;
     }
 
+    public static void yazdir(Object obj){
+        System.out.print(obj +" ");
+    }
+
 }
