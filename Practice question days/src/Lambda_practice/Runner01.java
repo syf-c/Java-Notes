ce1d41515c3ca4f52dd0ea7d35f870ef8717ba10
“User <musagurbuz4661@gmail.com>
2022-08-23 17:15:28 +0200

    Lambda Expression

--- Practice question days/src/Lambda_practice/Runner01.java
+++ Practice question days/src/Lambda_practice/Runner01.java
@@ -2,15 +2,24 @@ package Lambda_practice;
 
 public class Runner01 extends Lambda01 {
     public static void main(String[] args) {
-        AHarfiOlanlariSil(names);
-        System.out.println("**************************");
-        System.out.println("Enbuyuk Eleman : "+enBuyukEleman(numbers));
-        System.out.println("**************************");
-        System.out.println("Tum Elemanlarin Toplami : "+tumElemanlarinToplami(numbers));
-        System.out.println("**************************");
-        ciftElemanlarinCarpimi(numbers);
-        System.out.println("**************************");
-        System.out.println("**************************");
+
+        // aHarfleriSilenGerisiniYazdiran(names);
+        System.out.println("**************");
+        //  maxElemanBul(numbers);
+        System.out.println("**************");
+        //  elemanlariTopla(numbers);
+        System.out.println("**************");
+        //elemanlariCarp(numbers);
+        System.out.println("**************");
+        teklerinKareBuyuktenKucuge(numbers);
+        System.out.println("**************");
+        sonHarfeGoreTersSira(menu);
+        System.out.println("**************");
+        karakterSayiYedidenBuyuk(menu);
+        System.out.println("**************");
+        xIleBitenEleman(menu);
+        System.out.println();
+        karkterEnFazla(menu);
 
     }
 }
